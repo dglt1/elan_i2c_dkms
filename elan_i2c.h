e555752b9f1e15f602ebccd2939947a560c83246
dglt1 <47616132+dglt1@users.noreply.github.com>
2019-02-17 13:06:29 -0500

    updated vendor:product ID 04F3:3090
    added 04F3:3090 ID

--- elan_i2c.h
+++ elan_i2c.h
@@ -32,7 +32,7 @@
 /* IAP Firmware handling */
 #define ETP_PRODUCT_ID_FORMAT_STRING	"%d.0"
 #define ETP_FW_NAME		"elan_i2c_" ETP_PRODUCT_ID_FORMAT_STRING ".bin"
-#define ETP_IAP_START_ADDR	0x0083
+#define ETP_IAP_START_ADDR	0x3090
 #define ETP_FW_IAP_PAGE_ERR	(1 << 5)
 #define ETP_FW_IAP_INTF_ERR	(1 << 4)
 #define ETP_FW_PAGE_SIZE	64
